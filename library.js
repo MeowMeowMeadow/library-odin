352d6b71c6e345fe731f4e37b25291d2765ad0e0
Alex "Splitz" Davis <mead0wz0edavis@gmail.com>
2023-04-24 18:00:59 +0100

    Created Base Files
    Created the basic files for the site in question. Added some basic functions in the js file as well as setting up css on the site itself here.

---
+++ library.js
@@ -0,0 +1,27 @@
+let myLibrary = [
+    {
+        title:"A Game of Thrones",
+        author: "George R. R. Martin",
+        pages: "694",
+        readStatus: false
+    }
+];
+
+
+function Book(title, author, pages, readStatus)
+{
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.readStatus = readStatus
+}
+
+function addBooktoLibrary()
+{
+
+}
+
+function displayLibrary()
+{
+
+}
